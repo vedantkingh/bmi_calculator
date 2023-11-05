37204fd68d5dcf9244e93a8ac6ad0d3002102a50
Vedant <vedantkingh@gmail.com>
2023-11-05 15:19:28 +0530

    UI Refactoring

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,23 +18,23 @@ class _InputPageState extends State<InputPage> {
           Expanded(child: Row(
             children: [
               Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(colour: Color(0xFF1D1E33),),
               ),
               Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(colour: Color(0xFF1D1E33),),
               ),
             ],
           )),
           Expanded(
-            child: ReusableCard(),
+            child: ReusableCard(colour: Color(0xFF1D1E33),),
           ),
           Expanded(child: Row(
             children: [
               Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(colour: Color(0xFF1D1E33),),
               ),
               Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(colour: Color(0xFF1D1E33),),
               ),
             ],
           )),
@@ -45,16 +45,15 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    Key? key,
-  }) : super(key: key);
+  ReusableCard({required this.colour});
+  final Color colour;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
+          color: colour,
           borderRadius: BorderRadius.circular(10)
       ),
     );
